46888af33a6e034d773d10d0ae899b2be75f7c3b
sydykovadilet909-coder <sydykovadilet909@gmail.com>
2026-03-13 17:35:32 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let price = 250
+let products = 4
+const result = price * products
+console.log(`The total cost is: ${result}`)
+
+
+let a = 5;
+let b = 7;
+let sum = a + b;
+
+console.log("Сумма чисел:", sum);
+
+let prict = 100;      
+let quantity = 3;     
+let totalCost = price * quantity;
+
+console.log("Общая стоимость покупки:", totalCost);
+
+let num1 = 4;
+let num2 = 8;
+let num3 = 12;
+let average = (num1 + num2 + num3) / 3;
+
+console.log("Среднее арифметическое:", average);
+
+let length = 10;
+let width = 6;
+let perimeter = 2 * (length + width);
+
+console.log("Периметр прямоугольника:", perimeter);
+
+let x = 5;
+let y = 9;
+
+let temp = x;
+x = y;
+y = temp;
+
+console.log("x =", y);
+console.log("x =", x);
+
+
